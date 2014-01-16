7d6acd1a6b2b635e7499adef6e6cc56fd4760dfc
Daryl Hedley <darylhedley@hotmail.com>
2014-01-16 10:22:05 +0000

    Squashed commit of the following:
    commit 85623df475da627df027b39705785ef3863a42f6
    Author: Chris Steele <chris.steele@kineo.com>
    Date:   Wed Jan 15 11:35:07 2014 +0000
    
        made image part of hitarea
    
    commit b799d12f649f490a4ff8602609e4de97f822f1cb
    Author: Chris Steele <chris.steele@kineo.com>
    Date:   Wed Jan 15 11:27:14 2014 +0000
    
        gmcq first draft
    
    commit 3dc5cedd3b8351079b586823d340eaf56c0c3a8f
    Author: Chris Steele <chris.steele@kineo.com>
    Date:   Wed Jan 15 09:48:29 2014 +0000
    
        initial commit of files

---
+++ js/adapt-contrib-gmcq.js
@@ -0,0 +1,41 @@
+define(function(require) {
+    var Mcq = require('components/adapt-contrib-mcq/js/adapt-contrib-mcq');
+    var Adapt = require('coreJS/adapt');
+
+    var Gmcq = Mcq.extend({
+
+        events: {
+            'focus .gmcq-item input':'onItemFocus',
+            'blur .gmcq-item input':'onItemBlur',
+            'change .gmcq-item input':'onItemSelected',
+            "click .gmcq-widget .button.submit": "onSubmitClicked",
+            "click .gmcq-widget .button.reset": "onResetClicked",
+            "click .gmcq-widget .button.model": "onModelAnswerClicked",
+            "click .gmcq-widget .button.user": "onUserAnswerClicked"
+        },
+
+        canReset: function() {
+            return !this.$('.gmcq-widget, .button.reset').hasClass('disabled');
+        },
+
+        resetItems: function() { 
+            this.$('.gmcq-item label').removeClass('selected');
+            this.$('input').prop('checked', false);
+            this.deselectAllItems();
+            this.setAllItemsEnabled(true);
+        },
+
+        onItemSelected: function(event) {
+            var selectedItemObject = this.model.get('items')[$(event.currentTarget).parent('.gmcq-item').index()];
+            
+            if(this.model.get('_isEnabled') && !this.model.get('_isSubmitted')){
+                this.toggleItemSelected(selectedItemObject, event);
+            }
+        }
+    });
+    
+    Adapt.register("gmcq", Gmcq);
+
+    return Gmcq;
+    
+});
