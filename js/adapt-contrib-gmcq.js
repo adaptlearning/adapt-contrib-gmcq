284cc33a752acb2c0daf77871a85edf2327aed68
Guy Willis <guy.willis@kineo.com>
2019-01-04 16:27:56 +0000

    Converted tabs to spaces, indentation amends

--- js/adapt-contrib-gmcq.js
+++ js/adapt-contrib-gmcq.js
@@ -1,86 +1,80 @@
 define([
-    'core/js/adapt',
-    'components/adapt-contrib-mcq/js/adapt-contrib-mcq'
+  'core/js/adapt',
+  'components/adapt-contrib-mcq/js/adapt-contrib-mcq'
 ], function(Adapt, Mcq) {
 
-    var Gmcq = Mcq.view.extend({
+  var Gmcq = Mcq.view.extend({
 
-        events: {
-            'focus .gmcq-item input': 'onItemFocus',
-            'blur .gmcq-item input': 'onItemBlur',
-            'change .gmcq-item input': 'onItemSelected',
-            'keyup .gmcq-item input': 'onKeyPress'
-        },
+    events: {
+      'focus .gmcq-item input': 'onItemFocus',
+      'blur .gmcq-item input': 'onItemBlur',
+      'change .gmcq-item input': 'onItemSelected',
+      'keyup .gmcq-item input': 'onKeyPress'
+    },
 
-        onItemSelected: function(event) {
+    onItemSelected: function(event) {
+      var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.gmcq-item').index()];
 
-            var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.gmcq-item').index()];
+      if (this.model.get('_isEnabled') && !this.model.get('_isSubmitted')) {
+        this.toggleItemSelected(selectedItemObject, event);
+      }
+    },
 
-            if (this.model.get('_isEnabled') && !this.model.get('_isSubmitted')) {
-                this.toggleItemSelected(selectedItemObject, event);
-            }
+    setupQuestion: function() {
+      Mcq.view.prototype.setupQuestion.call(this);
 
-        },
+      this.listenTo(Adapt, {
+        'device:changed': this.resizeImage,
+        'device:resize': this.onDeviceResize
+      });
+    },
 
-        setupQuestion: function() {
-            Mcq.view.prototype.setupQuestion.call(this);
+    onQuestionRendered: function() {
+      this.resizeImage(Adapt.device.screenSize);
+      this.setUpColumns();
 
-            this.listenTo(Adapt, {
-                'device:changed': this.resizeImage,
-                'device:resize': this.onDeviceResize
-            });
+      this.$('label').imageready(_.bind(function() {
+        this.setReadyStatus();
+      }, this));
+    },
 
-        },
+    onDeviceResize: function() {
+      this.setUpColumns();
+    },
 
-        onQuestionRendered: function() {
+    resizeImage: function(width) {
+      var imageWidth = width === 'medium' ? 'small' : width;
 
-            this.resizeImage(Adapt.device.screenSize);
-            this.setUpColumns();
-
-            this.$('label').imageready(_.bind(function() {
-                this.setReadyStatus();
-            }, this));
-
-        },
-        
-        onDeviceResize: function() {
-            this.setUpColumns();
-        },
-
-        resizeImage: function(width) {
-            var imageWidth = width === 'medium' ? 'small' : width;
-
-            this.$('label').each(function(index) {
-                var $img = $(this).find('img');
-                var newSrc = $img.attr('data-' + imageWidth);
-                if (newSrc) {
-                    $img.attr('src', newSrc);
-                }
-            });
-
-        },
-
-        setUpColumns: function() {
-            var columns = this.model.get('_columns');
-
-            if (!columns) return;
-
-            if (Adapt.device.screenSize === 'large') {
-                this.$el.addClass('gmcq-column-layout');
-                this.$('.gmcq-item').css('width', (100 / columns) + '%');
-            } else {
-                this.$el.removeClass('gmcq-column-layout');
-                this.$('.gmcq-item').css('width', '');
-            }
+      this.$('label').each(function(index) {
+        var $img = $(this).find('img');
+        var newSrc = $img.attr('data-' + imageWidth);
+        if (newSrc) {
+          $img.attr('src', newSrc);
         }
+      });
+    },
 
-    }, {
-        template: 'gmcq'
-    });
+    setUpColumns: function() {
+      var columns = this.model.get('_columns');
 
-    return Adapt.register("gmcq", {
-        view: Gmcq,
-        model: Mcq.model.extend({})
-    });
+      if (!columns) return;
+
+      if (Adapt.device.screenSize === 'large') {
+        this.$el.addClass('gmcq-column-layout');
+        this.$('.gmcq-item').css('width', (100 / columns) + '%');
+      } else {
+        this.$el.removeClass('gmcq-column-layout');
+        this.$('.gmcq-item').css('width', '');
+      }
+    }
+
+  }, {
+    template: 'gmcq'
+  });
+
+  return Adapt.register("gmcq", {
+    view: Gmcq,
+    model: Mcq.model.extend({})
+  });
 
 });
