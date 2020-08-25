c1056fc73cdb1530f0d70ee5e96ccd9a0e625ad9
Jennifer Eiss <jennifer.eiss@kineo.com>
2020-08-25 16:51:31 +0100

    deleted unnecessary view method

--- js/gmcqView.js
+++ js/gmcqView.js
@@ -2,10 +2,6 @@ import McqView from 'components/adapt-contrib-mcq/js/mcqView';
 
 class GmcqView extends McqView {
 
-  setupQuestion(...args) {
-    super.setupQuestion(...args);
-  }
-
   onQuestionRendered() {
     this.$('.js-item-label').imageready(() => this.setReadyStatus());
   }
