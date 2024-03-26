dbaaaaeb3a64d5090f5eee4cdaa68620d26e7b47
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-26 11:16:40 +0000

    Added gmcq test

---
+++ test/e2e/gmcq.cy.js
@@ -0,0 +1,27 @@
+describe('Graphic Multiple Choice Question', function () {
+  beforeEach(function () {
+    cy.getData()
+    cy.visit('/');
+  });
+
+  it('should display the gmcq component', function () {
+    const gmcqComponents = this.data.components.filter((component) => component._component === 'gmcq')
+    gmcqComponents.forEach((gmcqComponent) => {
+      cy.visit(`/#/preview/${gmcqComponent._id}`);
+      const bodyWithoutHtml = gmcqComponent.body.replace(/<[^>]*>/g, '');
+      
+      cy.testQuestionButtons()
+      cy.testContainsOrNotExists('.gmcq__title', gmcqComponent.displayTitle)
+      cy.testContainsOrNotExists('.gmcq__body', bodyWithoutHtml)
+      cy.testContainsOrNotExists('.gmcq__instruction', gmcqComponent.instruction)
+      cy.get('.gmcq-item').should('have.length', gmcqComponent._items.length)
+      gmcqComponent._items.forEach((item) => {
+        cy.testContainsOrNotExists('.gmcq-item__text', item.text)
+      })
+      
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      cy.wait(1000)
+    });
+  });
+});
