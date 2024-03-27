d0dc62818edb4ab79ade95fc054314d80567fec9
Lemmy Adams <lemmy.adams@learningpool.com>
2024-03-27 17:55:25 +0000

    Revised custom commands

--- test/e2e/gmcq.cy.js
+++ test/e2e/gmcq.cy.js
@@ -8,18 +8,16 @@ describe('Graphic Multiple Choice Question', function () {
     const gmcqComponents = this.data.components.filter((component) => component._component === 'gmcq')
     gmcqComponents.forEach((gmcqComponent) => {
       cy.visit(`/#/preview/${gmcqComponent._id}`);
-      cy.stripHtml(gmcqComponent.body)
-      const bodyWithoutHtml = this.text;
+      const bodyWithoutHtml = cy.helpers.stripHtml(gmcqComponent.body)
       cy.testContainsOrNotExists('.gmcq__body', bodyWithoutHtml)
       
-      cy.testQuestionButtons()
       cy.testContainsOrNotExists('.gmcq__title', gmcqComponent.displayTitle)
       cy.testContainsOrNotExists('.gmcq__instruction', gmcqComponent.instruction)
       cy.get('.gmcq-item').should('have.length', gmcqComponent._items.length)
       gmcqComponent._items.forEach((item) => {
         cy.testContainsOrNotExists('.gmcq-item__text', item.text)
       })
-      
+
       // Make sure the current component is tested before moving to the next one
       // Custom cypress tests are async so we need to wait for them to pass first
       cy.wait(1000)
