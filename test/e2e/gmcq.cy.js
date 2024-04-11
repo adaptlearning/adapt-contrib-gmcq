e12dd0fe5551246c59f9ee9df4dc3754a7e08c1d
Lemmy Adams <lemmy.adams@learningpool.com>
2024-04-11 14:32:08 +0100

    PR feedback

--- test/e2e/gmcq.cy.js
+++ test/e2e/gmcq.cy.js
@@ -1,26 +1,26 @@
 describe('Graphic Multiple Choice Question', function () {
   beforeEach(function () {
-    cy.getData()
+    cy.getData();
     cy.visit('/');
   });
 
   it('should display the gmcq component', function () {
-    const gmcqComponents = this.data.components.filter((component) => component._component === 'gmcq')
-    gmcqComponents.forEach((gmcqComponent) => {
+    const gmcqComponents = this.data.components.filter(component => component._component === 'gmcq');
+    gmcqComponents.forEach(gmcqComponent => {
       cy.visit(`/#/preview/${gmcqComponent._id}`);
-      const bodyWithoutHtml = cy.helpers.stripHtml(gmcqComponent.body)
-      cy.testContainsOrNotExists('.gmcq__body', bodyWithoutHtml)
-      
-      cy.testContainsOrNotExists('.gmcq__title', gmcqComponent.displayTitle)
-      cy.testContainsOrNotExists('.gmcq__instruction', gmcqComponent.instruction)
-      cy.get('.gmcq-item').should('have.length', gmcqComponent._items.length)
-      gmcqComponent._items.forEach((item) => {
-        cy.testContainsOrNotExists('.gmcq-item__text', item.text)
-      })
+      const stripHtml = cy.helpers.stripHtml;
+      cy.testContainsOrNotExists('.gmcq__body', stripHtml(gmcqComponent.body));
+      cy.testContainsOrNotExists('.gmcq__title', stripHtml(gmcqComponent.displayTitle));
+      cy.testContainsOrNotExists('.gmcq__instruction', stripHtml(gmcqComponent.instruction));
+
+      cy.get('.gmcq-item').should('have.length', gmcqComponent._items.length);
+      gmcqComponent._items.forEach(item => {
+        cy.testContainsOrNotExists('.gmcq-item__text', item.text);
+      });
 
       // Make sure the current component is tested before moving to the next one
       // Custom cypress tests are async so we need to wait for them to pass first
-      cy.wait(1000)
+      cy.wait(1000);
     });
   });
 });
